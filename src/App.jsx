61c9d85c520a3064a08e4c25ce4ef5c26cde507b
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2024-03-24 14:56:56 -0500

    Install ESLint locally

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const emptyVariable = "";
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
